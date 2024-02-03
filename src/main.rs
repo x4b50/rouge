ab905dd0eb352148e10f40d5c924fa5dbfa6edc8
x4b50 <k.piwowarski@proton.me>
2024-02-03 23:07:17 +0100

    generate basic rooms

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
 use std::io::{stdout, Write, Stdout};
 use crossterm::{cursor::{MoveTo, MoveLeft}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, queue, style::Print, execute, event::{self, Event, KeyCode}};
+use rand::Rng;
 
 macro_rules! exit {
     ($stdout:expr, $msg:expr) => {
         execute!($stdout, LeaveAlternateScreen).unwrap();
         eprintln!($msg);
-        return;
+        return Err(());
     };
 }
 
-/*
+// /*
 // might use Rc if multiple things with same desc could be common
 enum Tile {
     Wall,
     Floor,
     Player,
     Door,
-    Enemy {desc: Box<str>},
-    Item {desc: Box<str>}
+    // Enemy {desc: Box<str>},
+    // Item {desc: Box<str>}
 }
 
+#[derive(Debug)]
 struct Room {
-    width: u8,
-    length: u8,
-    position: (u8, u8),
+    pos: Rect,
     // contents: Vec<(u8, u8, Tile)>
 }
 
@@ -40,6 +40,7 @@ enum Move {
 }
 // */
 
+#[derive(Debug)]
 struct Rect {
     x: u16,
     y: u16,
@@ -61,7 +62,7 @@ fn queue_rect(stdout: &mut Stdout, rect: &Rect) {
     }
 }
 
-fn main() {
+fn main() -> Result<(), ()>{
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
     terminal::enable_raw_mode().unwrap();
@@ -74,16 +75,44 @@ fn main() {
         exit!(stdout, "Height of terminal window should be at least 80 characters");
     }
 
-    // let mut grid: [[Option<Room>;3];3];
-    
-    let rect = Rect{
-        x: width/4,
-        y: height/4,
-        w: width/2,
-        h: height/2,
-    };
+    let mut grid: [[Option<Room>;3];3] = Default::default();
+    let mut room_count = 0;
+    while room_count < 6 {
+        grid = Default::default();
+        room_count = 0;
 
-    queue_rect(&mut stdout, &rect);
+        // // make them fit in the right "square" or generate them differnently
+        for (r, row) in (&mut grid).iter_mut().enumerate() {
+            for (c, column) in row.iter_mut().enumerate() {
+                if rand::random() {
+                    let r = r as u16;
+                    let c = c as u16;
+                    let x = rand::thread_rng().gen_range(c*width/3..(c+1)*width/3);
+                    let y = rand::thread_rng().gen_range(r*height/3..(r+1)*height/3);
+
+                    if (c+1)*width/3-x < 15 {continue};
+                    if (r+1)*height/3-y < 10 {continue};
+                    let w = rand::thread_rng().gen_range(12..(c+1)*width/3-x);
+                    let h = rand::thread_rng().gen_range(8..(r+1)*height/3-y);
+
+                    *column = Some(Room{pos: Rect {
+                        x, y,
+                        w, h,
+                    }});
+                    room_count += 1;
+                }
+            }
+        }
+    }
+
+    for row in &grid {
+        for column in row {
+            if let Some(r) = column {
+                queue_rect(&mut stdout, &r.pos);
+            }
+        }
+    }
+    
     stdout.flush().unwrap();
     execute!(stdout, MoveTo(1,1)).unwrap();
 
@@ -106,4 +135,6 @@ fn main() {
     }
     execute!(stdout, LeaveAlternateScreen).unwrap();
     terminal::disable_raw_mode().unwrap();
+
+    Ok(())
 }
