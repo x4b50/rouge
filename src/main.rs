8508a1482efc970f217c09ad73b7d00844f5aac5
x4b50 <k.piwowarski@proton.me>
2024-02-04 01:04:17 +0100

    add hallways

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{stdout, Write, Stdout};
-use crossterm::{cursor::{MoveTo, MoveLeft}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, queue, style::Print, execute, event::{self, Event, KeyCode}};
+use std::{io::{stdout, Write, Stdout}, usize};
+use crossterm::{cursor::{MoveTo, MoveLeft}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, queue, style::Print, execute, event::{self, Event, KeyCode}, style::{Attribute, SetAttribute, SetForegroundColor, Color, ResetColor, SetBackgroundColor}};
 use rand::Rng;
 
 macro_rules! exit {
@@ -62,6 +62,10 @@ fn queue_rect(stdout: &mut Stdout, rect: &Rect) {
     }
 }
 
+const ROOMS_X: u16 = 4;
+const ROOMS_Y: u16 = 3;
+const MIN_ROOM_COUNT: u32 = 8;
+
 fn main() -> Result<(), ()>{
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
@@ -75,9 +79,9 @@ fn main() -> Result<(), ()>{
         exit!(stdout, "Height of terminal window should be at least 80 characters");
     }
 
-    let mut grid: [[Option<Room>;3];3] = Default::default();
+    let mut grid: [[Option<Room>;ROOMS_X as usize];ROOMS_Y as usize] = Default::default();
     let mut room_count = 0;
-    while room_count < 6 {
+    while room_count < MIN_ROOM_COUNT {
         grid = Default::default();
         room_count = 0;
 
@@ -87,13 +91,13 @@ fn main() -> Result<(), ()>{
                 if rand::random() {
                     let r = r as u16;
                     let c = c as u16;
-                    let x = rand::thread_rng().gen_range(c*width/3..(c+1)*width/3);
-                    let y = rand::thread_rng().gen_range(r*height/3..(r+1)*height/3);
+                    let x = rand::thread_rng().gen_range(c*width/ROOMS_X..(c+1)*width/ROOMS_X);
+                    let y = rand::thread_rng().gen_range(r*height/ROOMS_Y..(r+1)*height/ROOMS_Y);
 
-                    if (c+1)*width/3-x < 15 {continue};
-                    if (r+1)*height/3-y < 10 {continue};
-                    let w = rand::thread_rng().gen_range(12..(c+1)*width/3-x);
-                    let h = rand::thread_rng().gen_range(8..(r+1)*height/3-y);
+                    if (c+1)*width/ROOMS_X-x < 15 {continue};
+                    if (r+1)*height/ROOMS_Y-y < 10 {continue};
+                    let w = rand::thread_rng().gen_range(12..(c+1)*width/ROOMS_X-x);
+                    let h = rand::thread_rng().gen_range(8..(r+1)*height/ROOMS_Y-y);
 
                     *column = Some(Room{pos: Rect {
                         x, y,
@@ -109,6 +113,51 @@ fn main() -> Result<(), ()>{
         for column in row {
             if let Some(r) = column {
                 queue_rect(&mut stdout, &r.pos);
+            }
+        }
+    }
+
+    macro_rules! add_hallway {
+        ($count:expr, $move1:expr, $move2:expr) => {
+            queue!(stdout,
+                   SetAttribute(Attribute::Bold),
+                   SetBackgroundColor(Color::Cyan),
+                   SetForegroundColor(Color::Black),
+                   $move1, Print(format!("{}", $count)),
+                   $move2, Print(format!("{}", $count)),
+                   SetAttribute(Attribute::Reset),
+                   ResetColor).unwrap();
+        };
+    }
+
+    let mut doors_count = 0;
+    for y in 0..ROOMS_Y {
+        for x in 0..ROOMS_X {
+            if let Some(room) = &grid[y as usize][x as usize] {
+                for x2 in x+1..ROOMS_X {
+                    if let Some(room2) = &grid[y as usize][x2 as usize] {
+                        doors_count += 1;
+                        add_hallway!(doors_count,
+                                     MoveTo(room.pos.x + room.pos.w-1, room.pos.y + room.pos.h/2),
+                                     if doors_count < 10 {
+                                         MoveTo(room2.pos.x, room2.pos.y + room2.pos.h/2)
+                                     } else {
+                                         MoveTo(room2.pos.x-1, room2.pos.y + room2.pos.h/2)
+                                     });
+                        break;
+                    }
+                }
+
+                for y2 in y+1..ROOMS_Y {
+                    if let Some(room2) = &grid[y2 as usize][x as usize] {
+                        doors_count += 1;
+                        add_hallway!(doors_count,
+                                     MoveTo(room.pos.x + room.pos.w/2, room.pos.y + room.pos.h-1),
+                                     MoveTo(room2.pos.x + room2.pos.w/2, room2.pos.y)
+                                     );
+                        break;
+                    }
+                }
             }
         }
     }
