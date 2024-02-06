4063f3bc7a16fcf6db4a61b8af7ab81d1f8991f2
x4b50 <k.piwowarski@proton.me>
2024-02-06 15:47:03 +0100

    added rangom items/enemies generation, now put them into rooms

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use core::panic;
 use std::{io::{stdout, Write, Stdout}, usize};
 use crossterm::{cursor::{MoveTo, MoveLeft}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, queue, style::Print, execute, event::{self, Event, KeyCode}, style::{Attribute, SetAttribute, SetForegroundColor, Color, ResetColor, SetBackgroundColor}};
-use rand::Rng;
+use rand::{Rng, random};
 
+// TODO: organise this code
+
+// TODO: work on that to not fuck up the display
 #[allow(unused_macros)]
 macro_rules! dprintln {
-    ($stdout:expr, $msg:expr) => {
+    ($stdout:expr, $( $msg:expr ),*) => {
         execute!($stdout, LeaveAlternateScreen).unwrap();
-        println!("{}", $msg);
+        terminal::disable_raw_mode().unwrap();
+        println!($( $msg, )*);
+        terminal::enable_raw_mode().unwrap();
         execute!($stdout, MoveLeft(u16::MAX)).unwrap();
         execute!($stdout, EnterAlternateScreen).unwrap();
     };
@@ -62,20 +68,33 @@ macro_rules! queue_position_cleanup {
     };
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-struct Room {
+#[derive(PartialEq, Clone)]
+struct Room<'a> {
     pos: Rect,
-    // contents: Vec<(u8, u8, Tile)>
+    contents: &'a [Object]
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(PartialEq)]
+struct Object {
+    x: u16,
+    y: u16,
+    content: Content
+}
+
+#[derive(PartialEq)]
+enum Content {
+    Item(Item),
+    Enemy(Enemy)
+}
+
+#[derive(Clone, Copy)]
 struct Hallway<'a> {
     entr: (Point, Point),
-    rooms: (&'a Room, &'a Room)
+    rooms: (&'a Room<'a>, &'a Room<'a>)
 }
 
 enum Move {
-    None,
+    NONE,
     R,
     U,
     D,
@@ -97,21 +116,40 @@ struct Point {
     y: u16
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Stat {
     HP,
     DEF,
     ATK,
     GOLD,
     EXP,
+    __Count,
+    NONE,
 }
 
+// TODO: might get reed of kind/effect and leave 1
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Item {
-    name: &'static str,
+    hidden: bool,
+    kind: ItemKind,
     effect: Stat,
     value: i32,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum ItemKind {
+    Healing,
+    Armor,
+    Weapon,
+    Gold,
+    EXP,
+    __Count,
+    NONE,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Enemy {
+    hidden: bool,
     kind: EnemyKind,
     hp: i32,
     def: i32,
@@ -119,11 +157,14 @@ struct Enemy {
     loot: Option<Item>,
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum EnemyKind {
     Zombie,
     Skeleton,
     Ghost,
     Ogre,
+    __Count,
+    NONE,
 }
 
 struct Player {
@@ -149,35 +190,158 @@ fn queue_rect(stdout: &mut Stdout, rect: &Rect) {
     }
 }
 
+// TODO: different level items
+impl Item {
+    fn random() -> Item {
+        let kind = unsafe {
+            std::mem::transmute::<u8, ItemKind>
+                (rand::thread_rng().gen_range(0..ItemKind::__Count as u8))
+        };
+        Item {
+            hidden: random(),
+            // name: ITEM_NAMES[rand::thread_rng().gen_range(0..ITEM_NAMES.len())],
+            kind,
+            effect: match kind {
+                ItemKind::Healing => Stat::HP,
+                ItemKind::Armor => Stat::DEF,
+                ItemKind::Weapon => Stat::ATK,
+                ItemKind::Gold => Stat::GOLD,
+                ItemKind::EXP => Stat::EXP,
+                ItemKind::__Count => panic!("`Item::random()` should not be able to generate kind 'ItemKind::__Count`'"),
+                ItemKind::NONE => panic!("`Item::random()` should not be able to generate kind 'ItemKind::NONE`'"),
+            },
+            value: rand::thread_rng().gen_range(1..10)
+        }
+    }
+}
+
+impl Default for Item {
+    fn default() -> Self {
+        Item {
+            hidden: false,
+            kind: ItemKind::NONE,
+            effect: Stat::NONE,
+            value: 0,
+        }
+    }
+}
+
+impl Default for Room<'static> {
+    fn default() -> Self {
+        Room {
+            pos: Rect {
+                x: 0, y: 0,
+                w: 0, h: 0,
+            },
+            contents: &[EMPTY_OBJECT],
+        }
+    }
+}
+
+// TODO: gen different stats based on type, balance a bit
+impl Enemy {
+    fn random() -> Enemy {
+        Enemy {
+            hidden: random(),
+            kind: unsafe {
+                std::mem::transmute
+                    (rand::thread_rng().gen_range(0..EnemyKind::__Count as u8))
+            },
+            hp: rand::thread_rng().gen_range(3..10),
+            def: rand::thread_rng().gen_range(3..10),
+            atk: rand::thread_rng().gen_range(3..10),
+            loot: if random() && random() {
+                Some(Item::random())
+            } else {None}
+        }
+    }
+}
+
+impl Default for Enemy {
+    fn default() -> Self {
+        Enemy {
+            hidden: false,
+            kind: EnemyKind::NONE,
+            hp: 0,
+            def: 0,
+            atk: 0,
+            loot: None,
+        }
+    }
+}
+
+impl Default for Object {
+    fn default() -> Self {
+        Object {
+            x: 0,
+            y: 0,
+            content: Content::Item(Item::default())
+        }
+    }
+}
+
+impl Object {
+    const fn const_default() -> Self {
+        Object {
+            x: 0,
+            y: 0,
+            // TODO: keep same as Item::default()
+            content: Content::Item(Item {
+                    hidden: false,
+                    kind: ItemKind::NONE,
+                    effect: Stat::NONE,
+                    value: 0,
+                })
+        }
+    }
+}
+
 const ROOMS_X: u16 = 4;
 const ROOMS_Y: u16 = 3;
 const MIN_ROOM_COUNT: u32 = 8;
 const HALLWAYS_SIZE: usize = ((ROOMS_X-1)*ROOMS_Y + ROOMS_X*(ROOMS_Y-1)) as usize;
+const EMPTY_OBJECT: Object = Object::const_default();
 const EMPTY_ROOM: Room = Room {
-    pos: Rect {x:0, y:0, w:0, h:0}
+    pos: Rect {x:0, y:0, w:0, h:0},
+    contents: &[EMPTY_OBJECT],
 };
+
 const CHAR_WALL: char = '#';
 const CHAR_PLAYER: char = '@';
+// const CHAR_HIDDEN: char = '?';
+// const CHAR_ITEM: char = 'I';
+// const CHAR_ENEMY_ZOMBIE: char = 'Z';
+// const CHAR_ENEMY_SKELETON: char = 'S';
+// const CHAR_ENEMY_GHOST: char = 'G';
+// const CHAR_ENEMY_OGRE: char = 'O';
+
+// TODO: make a shop mechanic or sth to make gold usefull
+const ITEMS_MAX: u32 = MIN_ROOM_COUNT*3/4;
+const ITEMS_MIN: u32 = ITEMS_MAX/3;
+const MONSTERS_MAX: u32 = MIN_ROOM_COUNT;
+const MONSTERS_MIN: u32 = MONSTERS_MAX/2;
+
+// const ITEM_NAMES: [&str; 4] = [
+    // "Armor",
+    // "Sword",
+    // "Healing kit",
+    // "Pile of gold",
+// ];
 
 fn main() -> Result<(), ()>{
     let mut stdout = stdout();
     let (width, height) = terminal::size().unwrap();
 
-    if width < 80 {
-        exit!(stdout, "Width of terminal window should be at least 80 characters");
-    }
-    if height < 30 {
-        exit!(stdout, "Height of terminal window should be at least 80 characters");
-    }
-
+    if width < 80 {exit!(stdout, "Width of terminal window should be at least 80 characters");}
+    if height < 30 {exit!(stdout, "Height of terminal window should be at least 80 characters");}
     let height = height-2;
 
-    let mut grid = [[EMPTY_ROOM;ROOMS_X as usize];ROOMS_Y as usize];
+    let mut grid: [[Room;ROOMS_X as usize];ROOMS_Y as usize] = Default::default();
     let mut room_count = 0;
     while room_count < MIN_ROOM_COUNT {
         for (r, row) in (&mut grid).iter_mut().enumerate() {
             for (c, column) in row.iter_mut().enumerate() {
-                if *column == EMPTY_ROOM && rand::random() {
+                if *column == EMPTY_ROOM && random() {
                     let r = r as u16;
                     let c = c as u16;
                     let x = rand::thread_rng().gen_range(c*width/ROOMS_X..(c+1)*width/ROOMS_X);
@@ -188,10 +352,13 @@ fn main() -> Result<(), ()>{
                     let w = rand::thread_rng().gen_range(12..(c+1)*width/ROOMS_X-x);
                     let h = rand::thread_rng().gen_range(8..(r+1)*height/ROOMS_Y-y);
 
-                    *column = Room{pos: Rect {
-                        x, y,
-                        w, h,
-                    }};
+                    *column = Room{
+                        pos: Rect {
+                            x, y,
+                            w, h,
+                        },
+                        contents: &[EMPTY_OBJECT]
+                    };
                     room_count += 1;
                 }
             }
@@ -262,9 +429,20 @@ fn main() -> Result<(), ()>{
         }
     }
     stdout.flush().unwrap();
+
+    let mut items =  [Item::default(); ITEMS_MAX as usize];
+    let items_cout = rand::thread_rng().gen_range(ITEMS_MIN..=ITEMS_MAX);
+    for i in 0..items_cout {items[i as usize] = Item::random()}
+
+    let mut enemies = [Enemy::default(); MONSTERS_MAX as usize];
+    let enemies_count = rand::thread_rng().gen_range(MONSTERS_MIN..=MONSTERS_MAX);
+    for i in 0..enemies_count {enemies[i as usize] = Enemy::random()}
+
+    // dprintln!(stdout, "items: {:#?}", items);
+    // dprintln!(stdout, "enemies: {:#?}", enemies);
     
     let mut position = Point{x:0, y:0};
-    let mut moved = Move::None;
+    let mut moved = Move::NONE;
     let mut curr_room = &EMPTY_ROOM;
 
     'pos: for y in 0..ROOMS_Y {
@@ -279,13 +457,12 @@ fn main() -> Result<(), ()>{
             }
         }
     }
-    if *curr_room == EMPTY_ROOM {unreachable!("How did it not find anything???")}
 
     // TODO: might switch up and down
     // TODO: simplify hallways
     'l: loop {
         match moved {
-            Move::None => {}
+            Move::NONE => {}
             Move::R => {
                 let next_pos = Point{x: position.x+1, y: position.y};
                 if next_pos.x < curr_room.pos.x + curr_room.pos.w-1 {
@@ -367,7 +544,7 @@ fn main() -> Result<(), ()>{
         stdout.flush().unwrap();
 
         // get event for next move
-        moved = Move::None;
+        moved = Move::NONE;
         if let Ok(e) = event::read() {
             match e {
                 Event::Key(k) => {
